a34336efe4a89127c1f9619bc4669b4443d8f647
raulrocha2 <raul.rocha2@gmail.com>
2022-09-07 13:18:29 -0300

    refactor: ensure SignUpController implement correct interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,7 @@
+import { IHttpRequest, IHttpResponse } from './protocols/i-http'
+
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: IHttpRequest): IHttpResponse {
     for (const field of ['name', 'email']) {
       if (!httpRequest.body[field]) {
         return {
