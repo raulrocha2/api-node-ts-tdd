006a05a952ee2aa67069e3c4c0ae59144ec5fc4b
raulrocha2 <raul.rocha2@gmail.com>
2022-09-28 20:10:28 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,12 @@
+import { IAccountModel } from '../../../../domain/models/i-account'
+import { IAddAccount, IAddAccountModel } from '../../../../domain/usecases/i-add-account'
+import { MongoHelper } from './helpers/mongo-helper'
+
+export class AccountMongoRepository implements IAddAccount {
+  async add (accountData: IAddAccountModel): Promise<IAccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const account = await accountCollection.findOne(result.insertedId)
+    return MongoHelper.map(account)
+  }
+}
---
+++ src/infra/db/mongodb/account-repository/helpers/mongo-helper.ts
@@ -0,0 +1,24 @@
+import { Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+
+  async connect (url: string): Promise<void> {
+    this.client = await MongoClient.connect(url)
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  map (collection: any): any {
+    const { _id, ...objWithoutId } = collection
+    return Object.assign({}, objWithoutId, {
+      id: _id.toString()
+    })
+  }
+}
