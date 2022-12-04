082a1a671b79a1134ddee6ab691265efab5d0d04
raulrocha2 <raul.rocha2@gmail.com>
2022-12-03 21:53:28 -0300

    test: ensure SurveyMongoRepository add survey on success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,39 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+
+let accountCollection: Collection
+describe('Survey Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('surveys')
+    await accountCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  test('Should add survey on success', async () => {
+    const sut = new SurveyMongoRepository()
+    await sut.add({
+      question: 'any_question',
+      answers: [
+        {
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+        {
+          answer: 'other_answer'
+        }
+      ]
+    })
+    const survey = await accountCollection.findOne({
+      question: 'any_question'
+    })
+    expect(survey).toBeTruthy()
+    expect(survey?.question).toBe('any_question')
+  })
+})
