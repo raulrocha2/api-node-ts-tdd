70532e4b49976a57c1513072cdf97ecb463d6d7c
raulrocha2 <raul.rocha2@gmail.com>
2022-12-10 19:53:13 -0300

    refactor: move makeDbAddAccount to a folder

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from '../../../../presentation/controllers/login/signup/signup-controller'
 import { IController } from '../../../../presentation/protocols'
 import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
-import { makeDbAddAccount } from '../../usecases/db-add-account-factory'
+import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory'
 import { makeDbAuthentication } from '../../usecases/db-authentication-factory'
 import { makeSignupValidation } from './signup-validation-factory'
 
