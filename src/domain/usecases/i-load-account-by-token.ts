d17ee9291b9d5af1fa7a05bbb226dffdcddb05b1
raulrocha2 <raul.rocha2@gmail.com>
2022-12-10 21:09:29 -0300

    feat: ensure AuthMiddleware call LoacAccountByToken with correct token

---
+++ src/domain/usecases/i-load-account-by-token.ts
@@ -0,0 +1,5 @@
+import { IAccountModel } from '../models/i-account'
+
+export interface ILoadAccountByToken {
+  load: (accessToken: string, role?: string) => Promise<IAccountModel>
+}
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,10 +1,18 @@
+import { ILoadAccountByToken } from '../../domain/usecases/i-load-account-by-token'
 import { AccessDeniedError } from '../errors'
 import { forbidden } from '../helpers/http/http-helper'
 import { IHttpRequest, IHttpResponse, IMiddleware } from '../protocols'
 
 export class AuthMiddleware implements IMiddleware {
+  constructor (
+    private readonly loadAccountByToken: ILoadAccountByToken
+  ) { }
+
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    const error = forbidden(new AccessDeniedError())
-    return await new Promise(resolve => resolve(error))
+    const accessToken = httpRequest.headers?.['x-access-token']
+    if (accessToken) {
+      await this.loadAccountByToken.load(accessToken)
+    }
+    return forbidden(new AccessDeniedError())
   }
 }
