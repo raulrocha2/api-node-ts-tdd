782c3221392d645f6e1c8a4dc7f54582fc2f16d3
raulrocha2 <raul.rocha2@gmail.com>
2022-12-12 10:42:08 -0300

    test: ensure DbLoadAccountByToken call Decrypter with correct values

---
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -0,0 +1,54 @@
+import { IAccountModel, IDecrypter, ILoadAccountByTokenRepository } from './db-load-account-by-token-protocols'
+import { DbLoadAccountByToken } from './db-load-account-by-token'
+
+const makeFakeAccount = (): IAccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'hashed_password',
+  accessToken: 'valid_token'
+})
+
+const makeLoadAccountByToken = (): ILoadAccountByTokenRepository => {
+  class LoadAccountByTokenRepositoryStub implements ILoadAccountByTokenRepository {
+    async load (accessToken: string, role?: string | undefined): Promise<IAccountModel> {
+      return await new Promise(resolve => resolve(makeFakeAccount()))
+    }
+  }
+  return new LoadAccountByTokenRepositoryStub()
+}
+
+const makeDecrtyperStub = (): IDecrypter => {
+  class DecrypterStub implements IDecrypter {
+    async decrypt (value: string): Promise<string> {
+      return await new Promise(resolve => resolve('any_value'))
+    }
+  }
+  return new DecrypterStub()
+}
+
+interface ISutTypes {
+  loadAccountByTokenRepositoryStub: ILoadAccountByTokenRepository
+  sut: DbLoadAccountByToken
+  decrypterStub: IDecrypter
+}
+
+const makeSut = (): ISutTypes => {
+  const loadAccountByTokenRepositoryStub = makeLoadAccountByToken()
+  const decrypterStub = makeDecrtyperStub()
+  const sut = new DbLoadAccountByToken(loadAccountByTokenRepositoryStub, decrypterStub)
+  return {
+    sut,
+    loadAccountByTokenRepositoryStub,
+    decrypterStub
+  }
+}
+
+describe('DbLoadAccountByToken Usecase', () => {
+  test('Should call Decrypter with correct values', async () => {
+    const { sut, decrypterStub } = makeSut()
+    const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
+    await sut.load('any_token')
+    expect(decryptSpy).toHaveBeenCalledWith('any_token')
+  })
+})
