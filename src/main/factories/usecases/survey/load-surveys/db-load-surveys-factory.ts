58cd4046f33b91f9ce2a008083eb7780062e2ca9
raulrocha2 <raul.rocha2@gmail.com>
2023-01-03 16:33:23 -0300

    feat: ensure route load surveys return 403 if access tokens is not provided

---
+++ src/main/factories/usecases/survey/load-surveys/db-load-surveys-factory.ts
@@ -0,0 +1,9 @@
+
+import { DbLoadSurveys } from '../../../../../data/usecases/load-surveys/db-load-surveys'
+import { ILoadSurveys } from '../../../../../domain/usecases/i-load-surveys'
+import { SurveyMongoRepository } from '../../../../../infra/db/mongodb/survey/survey-mongo-repository'
+
+export const makeDbLoadSurveys = (): ILoadSurveys => {
+  const surveyMongoRepository = new SurveyMongoRepository()
+  return new DbLoadSurveys(surveyMongoRepository)
+}
